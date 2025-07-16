e073bb0735ba0fcb03c6d88537708f3dddbd9e32
IvanTsve <iv_plamenovv@abv.bg>
2025-07-16 15:40:06 +0300

    Create index.tsx

---
+++ gof-cardio/src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+import MainTable from "@/components/mainTable";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+		<MainTable />
+
+	</div>
+  );
+}
