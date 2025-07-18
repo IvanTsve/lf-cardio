d123b79b1e3571123e0d7d7592995f079aea5133
IvanTsve <iv_plamenovv@abv.bg>
2025-07-18 15:47:08 +0300

    Update grid.tsx

--- gof-cardio/src/components/grid.tsx
+++ gof-cardio/src/components/grid.tsx
@@ -1,12 +1,30 @@
-function Grid(props : { gridSize: number }) {
-    let gridItems = [...Array(props.gridSize)].map((_,i) => (
-  <div key={i} className="w-[50px] h-[50px] border border-gray-300 flex items-center justify-center">
-  </div>
-));
+import Cube from "@/components/cube";
+import Inputs from "@/components/inputs";
+import { useState } from "react";
+import { GridSize } from "../types/types";
+function Grid() {
+    const [size, setSize] = useState([] as GridSize);
+    function setGridSize(data: number) {
+        if(data) {
+            setSize([...Array(data)].map(() => false) as GridSize);
+        }
+    }
+
+    function setIsAlive(index: number) {
+        setSize(prev => {
+            const updated = [...prev];  
+            updated[index] = !updated[index];
+            return [...updated];
+        });
+    }
+
     return (
-        <div className="grid grid-cols-6 gap-4">
-            {gridItems}
-        </div>
+        <>
+            <Inputs setGridSize={setGridSize} />
+            <div className="grid grid-cols-6 gap-4">
+            <Cube gridSize={size} setIsAlive={setIsAlive} />
+            </div>
+        </>
     )
 
 }
