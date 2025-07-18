85fb1e08f35459b4662fb115ebdaeaaa442f4d73
IvanTsve <iv_plamenovv@abv.bg>
2025-07-18 15:47:03 +0300

    Update cube.tsx

--- gof-cardio/src/components/cube.tsx
+++ gof-cardio/src/components/cube.tsx
@@ -1,8 +1,20 @@
-function Cube() {
-    return (
-        <div className="w-[10px] h-[10px] bg-blue-500 animate-spin">
+import { GridSize } from "@/types/types";
+function Cube(props: { gridSize: GridSize, setIsAlive: (index: number) => void }) {
 
-        </div>
+    function handleCellClick(index: number) {
+        props.setIsAlive(Number(index));
+    }
+
+
+
+    let gridItems = props.gridSize.map((isAlive,i) => (
+        <span key={i} className={`w-[78px] h-[78px] border border-gray-300 flex items-center justify-center cursor-pointer ${isAlive ? 'bg-green-500' : 'bg-red-500'}`} onClick={() => handleCellClick(i)}> {i}</span> 
+    ));
+
+    return (
+        <>
+            {gridItems}
+        </>
     )
 }
 
