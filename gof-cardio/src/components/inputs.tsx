bce574226d1821b30ab98cfda715f022780d6c0b
IvanTsve <iv_plamenovv@abv.bg>
2025-07-22 15:17:51 +0300

    Update inputs.tsx

--- gof-cardio/src/components/inputs.tsx
+++ gof-cardio/src/components/inputs.tsx
@@ -1,10 +1,14 @@
-import {InputsProps} from "../types/types";
+import {InputsProps, Play} from "../types/types";
 import { useState } from "react";
-
-function Inputs({setGridSize} : InputsProps) {
+type InputsComponentProps = InputsProps & Play;
+function Inputs({ setGridSize, play }: InputsComponentProps) {
     const [value, setValue] = useState(0);
     function handleSetSize() {
         setGridSize(value);
+    }
+
+    function handlePlay() {
+        play();
     }
 
     return (
@@ -14,6 +18,7 @@ function Inputs({setGridSize} : InputsProps) {
                 <input type="number" placeholder="Grid Size" className="border p-2 rounded"  value={value} onChange={(e) => setValue(Number(e.target.value))} />
                 <button className="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded" onClick={handleSetSize}>Set</button>
                 <button className="cursor-pointer bg-red-500 text-white px-4 py-2 rounded" onClick={() => setValue(0)}>Reset</button>
+                <button className="cursor-pointer bg-green-500 text-white px-4 py-2 rounded" onClick={() => handlePlay()}>play</button>
             </div>
         </div>
     )
