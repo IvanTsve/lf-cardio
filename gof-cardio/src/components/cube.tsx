54d0607b67230c01c38926acc9b975df8c12acd1
IvanTsve <iv_plamenovv@abv.bg>
2025-07-22 15:17:55 +0300

    Update cube.tsx

--- gof-cardio/src/components/cube.tsx
+++ gof-cardio/src/components/cube.tsx
@@ -1,15 +1,23 @@
 import { GridSize } from "@/types/types";
-function Cube(props: { gridSize: GridSize, setIsAlive: (index: number) => void }) {
+function Cube(props: { gridSize: GridSize, setIsAlive: (index: number, cellIndex: number) => void }) {
 
-    function handleCellClick(index: number) {
-        props.setIsAlive(Number(index));
+    function handleCellClick(row: number, cellIndex: number) {
+        props.setIsAlive(Number(row), Number(cellIndex));
     }
 
 
 
-    let gridItems = props.gridSize.map((isAlive,i) => (
-        <span key={i} className={`w-[78px] h-[78px] border border-gray-300 flex items-center justify-center cursor-pointer ${isAlive ? 'bg-green-500' : 'bg-red-500'}`} onClick={() => handleCellClick(i)}> {i}</span> 
-    ));
+    let gridItems = props.gridSize.map((row, rowIndex) =>
+        row.map((cell, cellIndex) => (
+            <span
+                key={cellIndex}
+                className={`w-[78px] h-[78px] border border-gray-300 flex items-center justify-center cursor-pointer transition ${cell ? 'bg-green-500' : 'bg-red-500'}`}
+                onClick={() => handleCellClick(rowIndex,cellIndex)}
+            >
+                {rowIndex} : {cellIndex}
+            </span>
+        ))
+    );
 
     return (
         <>
