3b1ce4e8d99fe71d83c68331837876f1b5d9574e
IvanTsve <iv_plamenovv@abv.bg>
2025-07-22 15:17:49 +0300

    Update types.ts

--- gof-cardio/src/types/types.ts
+++ gof-cardio/src/types/types.ts
@@ -1,5 +1,9 @@
-type GridSize = boolean[];
+type GridSize = boolean[][];
 type InputsProps = {
   setGridSize: (size: number) => void;
 };
-export type { GridSize, InputsProps };
+
+type Play = {
+    play: () => void;
+}
+export type { GridSize, InputsProps, Play };
